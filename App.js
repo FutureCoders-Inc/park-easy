c3cb442f6f0595dd1ce3204e70b0b16f8df18d29
Besa Salimusaj <besasalimusaj98@gmail.com>
2024-07-21 11:41:54 +0200

    Parking started

--- App.js
+++ App.js
@@ -1,20 +1,123 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  TouchableWithoutFeedback,
+  Keyboard,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform
+} from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}
+    >
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <View style={{ flex: 1 }}>
+          <View style={styles.header}>
+            <Text style={styles.title}>Sign Up</Text>
+          </View>
+          <View style={styles.container}>
+            <View style={styles.containerInput}>
+              <Text style={styles.label}>Name:</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Enter your full name..."
+                onChangeText={(value) => setName(value)}
+              />
+            </View>
+
+            <View style={styles.containerInput}>
+              <Text style={styles.label}>Email:</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Email..."
+                onChangeText={(value) => setName(value)}
+              />
+            </View>
+
+            <View style={styles.containerInput}>
+              <Text style={styles.label}>Password:</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Password..."
+                onChangeText={(value) => setName(value)}
+              />
+            </View>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Sign Up</Text>
+            </TouchableOpacity>
+            {/* <Button title="Sign Up" color="gray"></Button> */}
+          </View>
+        </View>
+      </ScrollView>
+    </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    padding: 10,
+    marginTop: 80, // Add margin to avoid overlap with header
+  },
+  containerInput: {
+    marginBottom: 30,
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 5,
+    textAlign: "left",
+  },
+  input: {
+    width: "100%",
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    paddingLeft: 10,
+  },
+  header: {
+    height: 80,
+    paddingTop: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    zIndex: 1, // Ensure the header is above other elements
+  },
+  title: {
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  button: {
+    backgroundColor: "#545458", // Example background color
+    padding: 15,
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
